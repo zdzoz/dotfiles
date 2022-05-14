b10f46f3856eceb5c8618df90927a09b9d90843c
Oskar Zdziarski <oskarzdz@gmail.com>
2022-05-14 03:31:57 -0700

    update nvimtree

--- nvim/lua/user/plugins/nvim-tree.lua
+++ nvim/lua/user/plugins/nvim-tree.lua
@@ -1,4 +1,7 @@
 require'nvim-tree'.setup {
+    git = {
+        ignore = false,
+    },
 }
 
 vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeFindFileToggle<CR>', { silent = true, noremap = true })
